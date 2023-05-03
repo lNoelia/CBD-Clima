45650bc22428ee31dd33c125055652daf775b577
alecarnun <alecarnun@alum.us.es>
2023-05-03 12:20:25 +0200

    Pagina en preparacion inicial

--- CBD/weather/views.py
+++ CBD/weather/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render 
-from django.http import HttpResponse
 import pymongo
 from django.views.decorators.http import require_http_methods
-from .models import ClimatologíaDiaria
+from .forms import *
 from django.conf import settings
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 #Define Db Name
@@ -24,11 +23,16 @@ def master(request):
 def index(request):
     return render(request,"index.html", {'STATIC_URL':settings.STATIC_URL})
 
-@require_http_methods("GET")
 def climatologiaDiariaList(request):
     piezasLista = climatologiaDiaria.find({}, limit=20)
-    context={'listaClimaDiario' : piezasLista, 'STATIC_URL':settings.STATIC_URL }
-    return render(request,"climaDiario/climatologiaDiaria.html",context)
+    search_form = SearchForm()
+    if request.method == 'POST':
+            search_form = SearchForm(request.POST)
+            if search_form.is_valid():
+                keyword = search_form.cleaned_data['keyword']
+                # piezasLista = climatologiaDiaria.find({'nombre': keyword}, limit=20)
+                piezasLista = climatologiaDiaria.find({'nombre': {'$regex':keyword}}, limit=20)
+    return render(request,"climaDiario/climatologiaDiaria.html",{'listaClimaDiario' : piezasLista, 'STATIC_URL':settings.STATIC_URL, 'search_form':search_form })
 
 @require_http_methods("GET")
 def estadisticas(request):
